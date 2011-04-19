ad18fd8ac3cca259e49be7006539a4fded1b2440
Christian Stigen Larsen <csl@sublevel3.org>
2011-04-20 01:23:47 +0200

    Minor changes

--- primitives.cpp
+++ primitives.cpp
@@ -1,7 +1,24 @@
+#include <stdexcept>
 #include "eval.h"
 #include "primops.h"
 #include "primitives.h"
 #include "print.h"
+
+// TODO: Move to another place
+std::string to_s(enum type_t type)
+{
+  switch ( type ) {
+  default:           return "?unknown"; break;
+  case NIL:          return "nil";      break;
+  case INTEGER:      return "integer";  break;
+  case CLOSURE:      return "cloure";   break;
+  case PAIR:         return "pair";     break;
+  case SYMBOL:       return "symbol";   break;
+  case STRING:       return "string";   break;
+  case U8VECTOR:     return "U8VECTOR"; break;
+  case CONTINUATION: return "continuation"; break;
+  }
+}
 
 cons_t* defun_print(cons_t *arg)
 {
@@ -55,16 +72,15 @@ cons_t* defun_add(cons_t *p)
 
   for ( ; p != NULL; p = cdr(p) ) {
      if ( stringp(p) )
-      continue; // TODO: throw error, or something
-
-    if ( integerp(p) )
+      throw std::runtime_error("Cannot add INTEGER and STRING");
+    else if ( integerp(p) )
       sum += p->integer;
-
-    if ( pairp(p) ) {
+    else if ( pairp(p) ) {
       cons_t *res = eval(car(p));
       if ( integerp(res) )
         sum += res->integer; // or else, thow (TOWO)
-    }
+    } else
+      throw std::runtime_error("Cannot add integer and " + to_s(type_of(p)));
   }
 
   return integer(sum);
