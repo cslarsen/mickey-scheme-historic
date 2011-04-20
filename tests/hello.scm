101f93821fc75e5d1f01bd3f6c29f8c4c35c550c
Christian Stigen Larsen <csl@sublevel3.org>
2011-04-20 14:29:57 +0200

    Nothing big

--- tests/hello.scm
+++ tests/hello.scm
@@ -3,4 +3,4 @@
 (display (string-append "Hello" ", " ; comment at end of line
                         "world!\n"
   ; will not be printed "<secret>"
-                        "\nFrom Mickey; Non-scheme language!\n\n"))
+                        "-- Mickey scheme\n\n"))
