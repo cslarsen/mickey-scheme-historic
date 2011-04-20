07ca305573a1b9878afd5814d8619aa40b12999a
Christian Stigen Larsen <csl@sublevel3.org>
2011-04-20 11:57:30 +0200

    Comment on meta-circular evaluator

--- apply.cpp
+++ apply.cpp
@@ -1,6 +1,12 @@
 #include <stdexcept>
 #include "apply.h"
 
+/*
+ * TODO: Add environment argument, and do this
+ *        - eval returns expr + environment
+ *        - apply gobbles expr + environment, returns environment
+ *        - eval gobbles up this new environment, etc
+ */
 cons_t* apply(lambda_t f, cons_t *args)
 {
   // TODO: Add environment argument
