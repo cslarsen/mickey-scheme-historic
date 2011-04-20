1e3079ce3f6c5037ad65a6a46bf0280c98f331be
Christian Stigen Larsen <csl@sublevel3.org>
2011-04-20 11:57:11 +0200

    Removed eval_print

--- eval.cpp
+++ eval.cpp
@@ -5,46 +5,6 @@
 #include "primitives.h"
 #include "apply.h"
 
-cons_t* eval_print(cons_t* p, int indent)
-{
-  if ( p == NULL ) {
-    printf(" <null>\n");
-    return p;
-  }
-
-  if ( indent > 0 )
-    printf(" ");
-
-  switch ( p->type ) {
-  case INTEGER: printf("integer %d\n", p->integer); break;
-  case CLOSURE: printf("closure\n"); break;
-  case SYMBOL: printf("symbol '%s'", p->symbol->name.c_str()); break;
-  case STRING: printf("string '%s'", p->string); break;
-  case U8VECTOR: printf("vector"); break;
-  case CONTINUATION: printf("continuation"); break;
-  case NIL: printf("nil"); break;
-  case PAIR: printf("pair"); break;
-  default: printf("unknown"); break;
-  }
-  printf("\n");
-
-  if ( p->type == PAIR ) {
-    for ( int n=0; n<indent; ++n )
-      printf(" ");
-    printf("car:");
-
-    eval_print(p->car, 1+indent);
-
-    for ( int n=0; n<indent; ++n )
-      printf(" ");
-    printf("cdr:");
-
-    eval_print(p->cdr, 1+indent);
-  }
-
-  return p;
-}
-
 lambda_t lookup_lambda(symbol_t *s)
 {
   // TODO: Put into table
