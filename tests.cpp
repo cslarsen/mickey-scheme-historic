a9892b5cdbaaf710edc4435bfb6f74d9a541fe75
Christian Stigen Larsen <csl@sublevel3.org>
2011-04-28 00:15:29 +0200

    More tests

--- tests.cpp
+++ tests.cpp
@@ -6,6 +6,9 @@
 #include "eval.h"
 #include "primitives.h"
 
+#define TEST_EVAL(expr, expect) TEST_STREQ(print(eval(parse(expr))), expect);
+#define TEST_PARSE(expr, expect) TEST_STREQ(sprint(parse(expr)), expect);
+
 static environment_t globals;
 
 cons_t* symbol(const char* s)
@@ -94,44 +97,80 @@ void run_tests()
   // (append nil (list 1 2))
   TEST_STREQ(sprint(cons(append(NULL, list(integer(1), integer(2))))), "(1 2)");
 
-  TEST_STREQ(sprint(parse("(cons 1 2)")), "(<closure> 1 2)");
-  TEST_STREQ(sprint(parse("(cOns 1 2)")), "(cOns 1 2)");
-  TEST_STREQ(sprint(parse("(CONS 1 2)")), "(CONS 1 2)");
-  TEST_STREQ(sprint(parse("(+ (* 1 2) 3)")), "(<closure> (<closure> 1 2) 3)");
-  TEST_STREQ(sprint(parse("(fx-+ (fx-* 1 2) 3)")), "(fx-+ (fx-* 1 2) 3)");
-  TEST_STREQ(sprint(parse("(1)")), "(1)");
-  TEST_STREQ(sprint(parse("((1))")), "((1))");
-  TEST_STREQ(sprint(parse("((1 2))")), "((1 2))");
-  TEST_STREQ(sprint(parse("((1 2) 3)")), "((1 2) 3)");
-  TEST_STREQ(sprint(parse("((a b) c)")), "((a b) c)");
-  TEST_STREQ(sprint(parse("(a (b c) d)")), "(a (b c) d)");
-  TEST_STREQ(sprint(parse("(a (B c) D)")), "(a (B c) D)");
-  TEST_STREQ(sprint(parse("(display (string-append \"Hello\" \", \" \"world!\"))")),
+  // parser
+  TEST_PARSE("(cons 1 2)", "(<closure> 1 2)");
+  TEST_PARSE("(cOns 1 2)", "(cOns 1 2)");
+  TEST_PARSE("(CONS 1 2)", "(CONS 1 2)");
+  TEST_PARSE("(+ (* 1 2) 3)", "(<closure> (<closure> 1 2) 3)");
+  TEST_PARSE("(fx-+ (fx-* 1 2) 3)", "(fx-+ (fx-* 1 2) 3)");
+  TEST_PARSE("(1)", "(1)");
+  TEST_PARSE("((1))", "((1))");
+  TEST_PARSE("((1 2))", "((1 2))");
+  TEST_PARSE("((1 2) 3)", "((1 2) 3)");
+  TEST_PARSE("((a b) c)", "((a b) c)");
+  TEST_PARSE("(a (b c) d)", "(a (b c) d)");
+  TEST_PARSE("(a (B c) D)", "(a (B c) D)");
+  TEST_PARSE("(display \"Hello\\nworld!\")))", "(<closure> \"Hello\\nworld!\")");
+  TEST_PARSE("a", "a");
+  TEST_PARSE("A", "A");
+  TEST_PARSE("(1 2 3) (4 5 6)", "(1 2 3) (4 5 6)");
+  TEST_PARSE("(1 2 3)\r\n(4 5 6)", "(1 2 3) (4 5 6)");
+  TEST_PARSE("(display (string-append \"Hello\" \", \" \"world!\"))",
     "(<closure> (<closure> \"Hello\" \", \" \"world!\"))");
-  TEST_STREQ(sprint(parse("(display \"Hello\\nworld!\")))")), "(<closure> \"Hello\\nworld!\")");
-  TEST_STREQ(sprint(parse("a")), "a");
-  TEST_STREQ(sprint(parse("A")), "A");
-  TEST_STREQ(sprint(parse("(1 2 3) (4 5 6)")), "(1 2 3) (4 5 6)");
-  TEST_STREQ(sprint(parse("(1 2 3)\r\n(4 5 6)")), "(1 2 3) (4 5 6)");
-  TEST_STREQ(print(eval(parse("(->string 123)"))), "123");
-  TEST_STREQ(print(eval(parse("(->string (list 1 2 (list 3 4)))"))), "1 2 (3 4)");
-  TEST_STREQ(print(eval(parse("(->string (list 1 2 (list 3 (list 4 4 5) 4)))"))), "1 2 (3 (4 4 5) 4)");
-  TEST_STREQ(print(eval(parse("(->string (list 1 2 (list 3 4 (* 3 30))))"))), "1 2 (3 4 90)");
 
-  TEST_STREQ(print(eval(parse("(display 123)"))), ""); // prints to console
-  TEST_STREQ(print(eval(parse("(display (+ 3 3 5))"))), ""); // prints to console
-  TEST_STREQ(print(eval(parse("(display \"hello\")"))), ""); // prints to console
-  TEST_STREQ(print(eval(parse("(+ 0)"))), "0");
-  TEST_STREQ(print(eval(parse("(+ 1)"))), "1");
-  TEST_STREQ(print(eval(parse("(+ 0 1)"))), "1");
-  TEST_STREQ(print(eval(parse("(+ 1 2)"))), "3");
-  TEST_STREQ(print(eval(parse("(+ 1 2 3)"))), "6");
-  TEST_STREQ(print(eval(parse("(+ -1)"))), "-1"); 
-  TEST_STREQ(print(eval(parse("(+ 5 -2 3)"))), "6"); 
-  TEST_STREQ(print(eval(parse("(+ -1 -3 -7)"))), "-11"); 
-  TEST_STREQ(print(eval(parse("(+ (* 5 5) 4)"))), "29"); 
-  TEST_STREQ(print(eval(parse("(+ (* 5 5) 4 12)"))), "41"); 
-  TEST_STREQ(print(eval(parse("(string-append \"one\" \"two\" \"three\")"))), "onetwothree");
+  // string operations
+  TEST_EVAL("(->string 123)", "123");
+  TEST_EVAL("(->string (list 1 2 (list 3 4)))", "1 2 (3 4)");
+  TEST_EVAL("(->string (list 1 2 (list 3 (list 4 4 5) 4)))", "1 2 (3 (4 4 5) 4)");
+  TEST_EVAL("(->string (list 1 2 (list 3 4 (* 3 30))))", "1 2 (3 4 90)");
+  TEST_EVAL("(string-append \"one\" \"two\" \"three\")", "onetwothree");
+
+  // side-effects / printing to console
+  TEST_EVAL("(display 123)", "");
+  TEST_EVAL("(display (+ 3 3 5))", "");
+  TEST_EVAL("(display \"hello\")", "");
+
+  // plus operator
+  TEST_EVAL("(+ 0)", "0");
+  TEST_EVAL("(+ 1)", "1");
+  TEST_EVAL("(+ 0 1)", "1");
+  TEST_EVAL("(+ 1 2)", "3");
+  TEST_EVAL("(+ 1 2 3)", "6");
+  TEST_EVAL("(+ -1)", "-1"); 
+  TEST_EVAL("(+ 5 -2 3)", "6"); 
+  TEST_EVAL("(+ -1 -3 -7)", "-11"); 
+  TEST_EVAL("(+ (* 5 5) 4)", "29"); 
+  TEST_EVAL("(+ (* 5 5) 4 12)", "41"); 
+
+  // predicates
+  TEST_EVAL("(integer?)", "#f");
+  TEST_EVAL("(integer? 1)", "#t");
+  TEST_EVAL("(integer? 0)", "#t");
+  TEST_EVAL("(integer? a)", "#f");
+  TEST_EVAL("(integer? 1.0)", "#f");
+  TEST_EVAL("(integer? 1.0f)", "#f");
+  TEST_EVAL("(integer? 10f)", "#f");
+  TEST_EVAL("(integer? 10)", "#t");
+  TEST_EVAL("(integer? 100)", "#t");
+  TEST_EVAL("(integer? 00)", "#t");
+  TEST_EVAL("(integer? 1234)", "#t");
+  TEST_EVAL("(integer? (list 1 2))", "#f");
+  TEST_EVAL("(integer? (+))", "#f");
+  TEST_EVAL("(integer? +)", "#f");
+  TEST_EVAL("(integer? -1)", "#t");
+  TEST_EVAL("(integer? -0)", "#t"); // works with mit-scheme, chicken scheme
+  TEST_EVAL("(integer? -123)", "#t");
+  TEST_EVAL("(integer? -123.0)", "#f");
+  TEST_EVAL("(integer? ())", "#f");
+
+  TEST_EVAL("(procedure?)", "#f");
+  TEST_EVAL("(procedure? 123)", "#f");
+  TEST_EVAL("(procedure? abba-rules)", "#f");
+  TEST_EVAL("(procedure? procedure?)", "#t");
+  TEST_EVAL("(procedure? +)", "#t");
+  TEST_EVAL("(procedure? list)", "#t");
+  TEST_EVAL("(procedure? (list 1 2))", "#f");
+  TEST_EVAL("(procedure? (+ 1 2))", "#f");
 
   results();
 }
