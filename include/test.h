2fbf340fc86adbd6d99ea375c144eca65facccf7
Christian Stigen Larsen <csl@sublevel3.org>
2011-04-28 18:06:36 +0200

    Wrap in try-catch

--- include/test.h
+++ include/test.h
@@ -1,8 +1,10 @@
+#include <stdexcept>
 #include <string>
 
-#define TEST_TRUE(expr) { test(expr, #expr); }
-#define TEST_FALSE(expr) { test(expr == false, #expr); }
-#define TEST_STREQ(expr, expected) { test_streq(#expr, expr, expected); }
+#define CATCH_ALL(expr) { try { expr; } catch (const std::exception& e) { fprintf(stderr, "  Exception: %s\n", e.what()); } }
+#define TEST_TRUE(expr) { CATCH_ALL(test(expr, #expr);) }
+#define TEST_FALSE(expr) { CATCH_ALL(test(expr == false, #expr);) }
+#define TEST_STREQ(expr, expected) { CATCH_ALL(test_streq(#expr, expr, expected);) }
 
 void test_streq(const std::string& code, const std::string& actual, const std::string& expected);
 void test(bool result, const char *descr);
