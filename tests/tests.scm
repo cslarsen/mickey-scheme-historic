43d856a311d88532dc4774eebb4d3175eb606774
Christian Stigen Larsen <csl@sublevel3.org>
2011-05-01 10:39:27 +0200

    More tests

--- tests/tests.scm
+++ tests/tests.scm
@@ -2,7 +2,6 @@
 ;; Define some test functions
 ;;
 
-
 (define tests 0)
 (define test+1
   (lambda (_foo)
@@ -13,13 +12,14 @@
   (lambda (_foo)
     (set! failed (+ failed 1))))
 
-(define fail (lambda (code expected)
+(define fail (lambda (code expected actual)
   (begin
     (test+1 (quote _))
     (fail+1 (quote _))
     (display (string-append
       (->string tests) " FAIL: "
-        (->string code) " != " (->string expected) "\n")))))
+        (->string code) " != " (->string expected) "\n"
+        "  Actual result: '" (->string actual) "'\n")))))
 
 (define success (lambda (code expected)
   (begin
@@ -32,7 +32,7 @@
   (begin
     (if
       (not (eq? (eval code) expected))
-        (fail code expected)
+        (fail code expected (eval code))
         (success code expected)))))
 
 (define results (lambda (__dummy__)
@@ -50,6 +50,15 @@
 (test-eq (quote (+ 1 2 3 4)) 10)
 (test-eq (quote (* 1 2 3 4)) 24)
 (test-eq (quote (- 1 2 3 4)) -8)
+(test-eq (quote (string-append "a" "b" "b" "a")) "abba")
+(test-eq (quote (->string 12345)) "12345")
+(test-eq (quote (->string #f)) "#f")
+(test-eq (quote (->string #t)) "#t")
+(test-eq (quote (->string (eq? 1 2))) "#f")
+(test-eq (quote (->string (eq? 3 (+ 1 2)))) "#t")
+(test-eq (quote (->string (eq? 3 (+ 1 2 3)))) "#f")
+(test-eq (quote (->string (list 1 2 (list 3 4)))) "(1 2 (3 4))")
+(test-eq (quote (atom? (quote abba))) #t)
 
 (display "\nResults\n")
 (results (quote __dummy__))
