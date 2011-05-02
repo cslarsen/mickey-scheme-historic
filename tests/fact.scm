e2f09932e17021c3986e86c4fe8c60670fac9b40
Christian Stigen Larsen <csl@sublevel3.org>
2011-05-02 21:39:35 +0200

    Factorial

---
+++ tests/fact.scm
@@ -0,0 +1,25 @@
+; Standard factorial function,
+; from Queinnec, p. 54
+(define (fact n)
+  (if (= n 0) 1
+      (* n (fact (- n 1)))))
+
+(define (show-fact n)
+  (display (string-append
+    (->string n) "! = " (->string (fact n)) "\n")))
+
+(show-fact 0)
+(show-fact 1)
+(show-fact 2)
+(show-fact 3)
+(show-fact 4)
+(show-fact 5)
+(show-fact 6)
+(show-fact 7)
+(show-fact 8)
+(show-fact 9)
+(show-fact 10)
+(show-fact 20)
+(show-fact 40)
+(show-fact 80)
+(show-fact 100)
