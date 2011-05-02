57b3d99543c5688a566ee7c3e97fd1e84f4b4e20
Christian Stigen Larsen <csl@sublevel3.org>
2011-05-02 22:12:36 +0200

    Better test

--- tests/loop1.scm
+++ tests/loop1.scm
@@ -1,11 +1,11 @@
 ;; Testing simple recursion loop
 
-(define loop
-  (lambda (n)
-    (display (string-append "n=" (->string n) "\n"))
-    (if (< n 2) n
-        (loop (- n 1)))))
+(define (loop n)
+  (display (string-append (->string n) " "))
+  (if (= n 1)
+      (display "\n") ; stop
+      (loop (- n 1))))
 
-(display "Printing numbers 1 to 100\n")
+(display "Printing numbers from 100 to 1\n")
 (loop 100)
 (display "Done\n")
