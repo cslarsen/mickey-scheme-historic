422304003422b660de2689735c4a71183a2ebd60
Christian Stigen Larsen <csl@sublevel3.org>
2011-05-05 00:27:48 +0200

    SICP tests (incomplete)

---
+++ tests/sicp-tests.scm
@@ -0,0 +1,91 @@
+;; Mickey Scheme
+;;
+;; Test code from SICP
+;;
+
+(load "test.scm")
+
+(display "Tests from\n")
+(display "Structure and Interpretation of Comptuter Programs (SICP)\n\n")
+
+;; ---
+
+(display "SICP SECTION 1.1.1\n")
+
+(test-eq (quote 486) 486)
+(test-eq (quote (+ 137 349)) 486)
+(test-eq (quote (- 1000 334)) 666)
+(test-eq (quote (* 5 99)) 495)
+(test-eq (quote (/ 10 5)) 2)
+(test-eq (quote (+ 2.7 10)) 12.7)
+(test-eq (quote (+ 21 35 12 7)) 75)
+(test-eq (quote (* 25 4 12)) 1200)
+(test-eq (quote (+ (* 3 5) (- 10 6))) 19)
+(test-eq (quote (+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))) 57)
+(test-eq (quote 
+ (+ (* 3
+       (+ (* 2 4)
+          (+ 3 5)))
+    (+ (- 10 7)
+       6)))
+  57) ; expected
+
+;; ---
+
+(display "\nSICP SECTION 1.1.2\n")
+
+(define size 2)
+(test-eq (quote size) 2)
+(test-eq (quote (* 5 size)) 10)
+
+(define pi 3.14159)
+(define radius 10)
+(test-eq (quote (* pi (* radius radius))) 314.158997)
+
+(define circumference (* 2 pi radius))
+(test-eq (quote circumference) 62.831802)
+
+;; ---
+
+(display "\nSICP SECTION 1.1.3\n")
+
+(test-eq (quote (* (+ 2 (* 4 6)))) 26)
+(test-eq (quote (+ 3 5 7)) 15)
+
+;; ---
+
+(display "\nSICP SECTION 1.1.4\n")
+
+(define (square x) (* x x))
+(test-eq (quote (square 21)) 441)
+(test-eq (quote (square (+ 2 5))) 49)
+(test-eq (quote (square (square 3))) 81)
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(test-eq (quote (sum-of-squares 3 4)) 25)
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+(test-eq (quote (f 5)) 136)
+
+;; ---
+
+(display "\nSICP SECTION 1.1.5\n")
+
+(test-eq (quote (f 5)) 136)
+(test-eq (quote (sum-of-squares (+ 5 1) (* 5 2))) 136)
+(test-eq (quote (+ (square 6) (square 10))) 136)
+(test-eq (quote (+ (* 6 6) (* 10 10))) 136)
+(test-eq (quote (+ 36 100)) 136)
+
+(test-eq (quote (f 5)) 136)
+(test-eq (quote (sum-of-squares (+ 5 1) (* 5 2))) 136)
+(test-eq (quote (+    (square (+ 5 1))      (square (* 5 2))  )) 136)
+(test-eq (quote (+    (* (+ 5 1) (+ 5 1))   (* (* 5 2) (* 5 2)))) 136)
+(test-eq (quote (+         (* 6 6)             (* 10 10))) 136)
+(test-eq (quote (+           36                   100)) 136)
+(test-eq (quote                     136) 136)
+
+(display "\nRESULTS\n")
+(results)
---
+++ tests/test.scm
@@ -0,0 +1,42 @@
+;; Library for performing tests
+
+(define tests 0)
+(define test+1
+  (lambda (_foo)
+    (set! tests (+ tests 1))))
+
+(define failed 0)
+(define fail+1
+  (lambda (_foo)
+    (set! failed (+ failed 1))))
+
+(define fail (lambda (code expected actual)
+  (begin
+    (test+1 (quote _))
+    (fail+1 (quote _))
+    (display (string-append
+      (->string tests) " FAIL: "
+        (->string code) " != " (->string expected) "\n"
+        "  Actual result: '" (->string actual) "'\n")))))
+
+(define success (lambda (code expected)
+  (begin
+    (test+1 (quote _))
+    (display (string-append
+      (->string tests) " OK: "
+        (->string code) " == " (->string expected) "\n")))))
+
+(define test-eq (lambda (code expected)
+  (begin
+    (if
+      (not (eq? (eval code) (eval expected)))
+        (fail code expected (eval code))
+        (success code expected)))))
+
+(define results (lambda ()
+    (display 
+      (string-append
+        (->string (- tests failed)) " / " (->string tests) " tests OK, "
+          (->string failed) " failed\n"))))
+
+
