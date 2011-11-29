6520ee0f0bdc942b7b55ab41c07bf922e9d678ca
Christian Stigen Larsen <csl@sublevel3.org>
2011-11-29 18:14:18 +0100

    Added another syntax-rules test

---
+++ tests/macro2.scm
@@ -0,0 +1,14 @@
+(display "Should get (2 1) now: ")
+
+(display
+(let ((x 1) (y 2))
+  (define-syntax swap!
+    (syntax-rules ()
+      ((swap! a b)
+       (let ((tmp a))
+         (set! a b)
+         (set! b tmp)))))
+  (swap! x y)
+  (list x y)))
+
+(newline)
