ad72a862541088fa5156626ab70a8a30400f7d7b
Christian Stigen Larsen <csl@sublevel3.org>
2012-07-26 23:06:13 +0200

    Bug: Rest args is not matched in syntax-rules

---
+++ tests/breaks-mickey/test-syntax-rest-args.scm
@@ -0,0 +1,12 @@
+(define-syntax hello
+  (syntax-rules ()
+    ((_ foo . bar)
+     (begin
+       (display "foo=")
+       (display (quote foo))
+       (display " bar=")
+       (display (quote bar))
+       (newline)))))
+
+(hello world) ; ==> foo=world bar=()
+(hello there brave new world!) ; ==> foo=there bar=(brave new world!)
