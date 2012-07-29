0056a952246cd07016c0a13df78514861bfdb8de
Christian Stigen Larsen <csl@sublevel3.org>
2012-07-29 16:29:09 +0200

    Updated year

--- src/print.cpp
+++ src/print.cpp
@@ -1,12 +1,12 @@
 /*
  * Mickey Scheme
  *
- * Copyright (C) 2011 Christian Stigen Larsen <csl@sublevel3.org>
+ * Copyright (C) 2011-2012 Christian Stigen Larsen <csl@sublevel3.org>
  * http://csl.sublevel3.org                              _
  *                                                        \
  * Distributed under the modified BSD license.            /\
  * Please post bugfixes and suggestions to the author.   /  \_
- *                                                          
+ *
  */
 
 #include "print.h"
