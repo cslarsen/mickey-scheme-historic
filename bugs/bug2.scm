c26841e1991809c5a024271797d4c6f85337ce11
Christian Stigen Larsen <csl@sublevel3.org>
2012-08-08 00:04:36 +0200

    Typo

--- bugs/bug2.scm
+++ bugs/bug2.scm
@@ -7,8 +7,7 @@
         (rename (scheme write)
                 (display print-this)))
 
-(define (dontwanna)
-  #f)
+(define dontwanna #f)
 
 (print-this "And ALSO this ==> ")
-(print-this (base:my-unless dontwanna "YEAH!"))
+(print-this (base:my-unless dontwanna "YEAH!\n"))
