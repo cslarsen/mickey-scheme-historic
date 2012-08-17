bf0e3c238266e1339e761b36ff303dc2a14bc96a
Christian Stigen Larsen <csl@sublevel3.org>
2012-08-18 00:11:08 +0200

    Using the LGPL 2.1+

--- lib/base.scm
+++ lib/base.scm
@@ -4,9 +4,9 @@
  | Copyright (C) 2012 Christian Stigen Larsen <csl@sublevel3.org>
  | http://csl.sublevel3.org                              _
  |                                                        \
- | Distributed under the modified BSD license.            /\
+ | Distributed under the LGPL 2.1; see LICENSE            /\
  | Please post bugfixes and suggestions to the author.   /  \_
- |                                                          
+ |
  |#
 
 #|
